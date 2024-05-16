3855673e12aed38030b0f725859f4b4d14a7d8be
SainexDo <ventizgene@gmail.com>
2024-05-16 15:55:55 -0600

    final del dia

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+// const matriz = [
+//     [1, 2, 3]
+//     [4, 5, 6]
+//     [7, 8, 9]
+// ];
+
+// console.log(matriz);
+
+// matriz[0][0] = "X"
+
+// console.log(matriz);
+
+// //Aquí lo que se estuvo haciendo es mostrar las coordenadas, de hecho,
+// //si marco posiciones [1][2] = "O" se marcará una "O" en la posición 7.
